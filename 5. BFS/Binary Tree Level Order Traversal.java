2410cce8b0e8e5ce0093d882c43d058aab339585
Zhang Dong <zhangdong.1995@outlook.com>
2020-06-18 16:25:15 -0400

    Binary Tree Level Order Traversal

---
+++ 5. BFS/Binary Tree Level Order Traversal.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new LinkedList<List<Integer>>();
+        if (root == null) return res;
+        
+        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        q.offer(root);
+        while (!q.isEmpty()) {
+            int size = q.size();
+            List<Integer> curLevel = new LinkedList<Integer>();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+                if (node.left != null) q.offer(node.left);
+                if (node.right != null) q.offer(node.right);
+                curLevel.add(node.val);
+            }
+            res.add(curLevel);
+        }
+        return res;
+    }
+}
