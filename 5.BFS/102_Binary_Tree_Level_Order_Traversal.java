92cfe94cc38e5bf2eb10ad07b55d59b1ec5f7d80
Zhang Dong <zhangdong.1995@outlook.com>
2020-06-18 16:41:35 -0400

    update readme

--- 5.BFS/102_Binary_Tree_Level_Order_Traversal.java
+++ 5.BFS/102_Binary_Tree_Level_Order_Traversal.java
@@ -1,3 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> res = new LinkedList<List<Integer>>();
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# Algorithm_solution
-Solutions for my LeetCode challenging
+# CrackingAlgorithms
+Solutions for my LeetCode challenging and Java practice.
+
+# Problems & Solutions
+
+| # | Title | Solution | Basic idea (One line) |
+|---| ----- | -------- | --------------------- |
+| 102 | [Binary Tree Level Order Traversal](https://leetcode.com/problems/binary-tree-level-order-traversal/) | [Java](https://github.com/zdong/CrackingAlgorithm/5.BFS/102_Binary_Tree_Level_Order_Traversal)| BFS |
