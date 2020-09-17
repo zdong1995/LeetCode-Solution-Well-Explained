47e6a79e7976943c21b938ad779aab12f149d0d5
Zhang Dong <zhangdong.1995@outlook.com>
2020-09-17 09:52:09 -0400

    iterative pre order traverse

--- Tree/binary-tree-preorder-traversal.java
+++ Tree/binary-tree-preorder-traversal.java
@@ -1,3 +1,4 @@
+// recursion
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> res = new ArrayList<>();
@@ -16,5 +17,30 @@ class Solution {
         list.add(root.val);
         helper(list, root.left);
         helper(list, root.right);
+    }
+}
+
+// iterative
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        stack.offerFirst(root);
+        while (!stack.isEmpty()) {
+            TreeNode cur = stack.pollFirst();
+            res.add(cur.val);
+            // every time we poll starting from left to right
+            // thus we need offer from right to left
+            if (cur.right != null) {
+                stack.offerFirst(cur.right);
+            }
+            if (cur.left != null) {
+                stack.offerFirst(cur.left);
+            }
+        }
+        return res;
     }
 }
