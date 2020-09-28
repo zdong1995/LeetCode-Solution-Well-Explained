5b91cfda6e4886c7a2a19919ede7fa22736b8bca
Zhang Dong <zhangdong.1995@outlook.com>
2020-09-28 14:41:33 -0400

    two sum unique pairs

---
+++ TwoPointers/two-sum-unique-pairs.java
@@ -0,0 +1,35 @@
+public class Solution {
+  /**
+   * @param nums: an array of integer
+   * @param target: An integer
+   * @return: An integer
+   */
+  public int twoSum6(int[] nums, int target) {
+      if (nums == null || nums.length == 0) {
+          return 0;
+      }
+      Arrays.sort(nums);
+      int count = 0;
+      int left = 0;
+      int right = nums.length - 1;
+      while (left < right) {
+          int sum = nums[left] + nums[right];
+          if (sum == target) {
+              count++;
+              left++;
+              right--;
+              while (left < right && nums[left] == nums[left - 1]) {
+                  left++;
+              }
+              while (left < right && nums[right] == nums[right + 1]) {
+                  right--;
+              }
+          } else if (sum > target) {
+              right--;
+          } else {
+              left++;
+          }
+      }
+      return count;
+  }
+}
