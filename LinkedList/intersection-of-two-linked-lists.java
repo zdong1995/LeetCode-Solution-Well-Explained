f067be313b8b10c8f5575d8db274864b50758529
Zhang Dong <zhangdong.1995@outlook.com>
2020-11-28 23:53:51 -0500

    intersection of two linkedlist

---
+++ LinkedList/intersection-of-two-linked-lists.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode a = headA, b = headB;
+
+        while (a != b) {
+            a = a == null ? headB : a.next;
+            b = b == null ? headA : b.next;
+        }
+        return a;
+    }
+}
