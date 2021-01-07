cf6e119600d493d9bf79a731471d1a666874dca3
Zhang Dong <zhangdong.1995@outlook.com>
2021-01-07 00:54:20 -0500

    iterative multilevel tree

--- Tree/populating-next-right-pointers-in-each-node-ii.java
+++ Tree/populating-next-right-pointers-in-each-node-ii.java
@@ -1,4 +1,5 @@
 class Solution {
+    // Method 1: BFS
     public Node connect(Node root) {
         if (root == null) {
             return root;
@@ -21,6 +22,38 @@ class Solution {
                     queue.offer(cur.right);
                 }
             }
+        }
+        return root;
+    }
+
+    // Method 2: Iterative
+    public Node connect2(Node root) {
+        if (root == null) {
+            return null;
+        }
+        Node leftMost = root;
+        while (leftMost != null) {
+            // start from leftMost node to traverse current level
+            // and connect the next level
+            Node cur = leftMost; // current node at current level
+            Node dummy = new Node(0); // used to store head of next level
+            Node prev = dummy; // the leading node of next level
+            // use dummy to avoid null check to simplify the implementation
+            while (cur != null) {
+                // traverse current level, to connect prev node to next node in the next level
+                if (cur.left != null) {
+                    prev.next = cur.left;
+                    prev = prev.next;
+                }
+                if (cur.right != null) {
+                    prev.next = cur.right;
+                    prev = prev.next;
+                }
+                // move to next node in current level
+                cur = cur.next;
+            }
+            // move leftMost to left most node of next level, i.e. the head of next level
+            leftMost = dummy.next;
         }
         return root;
     }
